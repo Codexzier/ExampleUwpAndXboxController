394461361be0f92b00ab9308fb9c57d2b5aa807e
Codexzier <johannes.paul.langner@live.de>
2018-04-10 00:29:58 +0200

    Example app to use xbox controller
    A simple app to use your xbox controller with a UWP. It running on PC,
    Tablet and Raspberry Pi 2/3

---
+++ ExampleUwpAndXboxController/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace ExampleUwpAndXboxController
+{
+    public sealed partial class MainPage : Page
+    {
+private MainViewModel _viewModel = new MainViewModel();
+
+/// <summary>
+/// Ermöglicht eine Schleife Asynchron zum Haupteil der Anwendung auszuführen.
+/// </summary>
+private Task _run;
+
+/// <summary>
+/// Hauptklasse für das einlesen des Xbox Controllers.
+/// </summary>
+private Gamepad _gamePad;
+
+/// <summary>
+/// Wird nur für den zufälligen Farbwechsel verwendet.
+/// </summary>
+private Random _random = new Random();
+
+public MainPage()
+{
+    this.InitializeComponent();
+
+    this.DataContext = this._viewModel;
+
+    Gamepad.GamepadAdded += this.Gamepad_GamepadAdded;
+    Gamepad.GamepadRemoved += this.Gamepad_GamepadRemoved;
+
+    this.MovingPoint.Fill = new SolidColorBrush(Colors.Black);
+}
+
+/// <summary>
+/// Wenn der Xbox Controller getrennt wird.
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+private void Gamepad_GamepadRemoved(object sender, Gamepad e) => this._gamePad = null;
+
+/// <summary>
+/// Wird ausgeführt, wenn ein Xbox Controller verbunden wurde.
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e">Übergibt die Instanz der verbundenen Xbox Controllers.</param>
+private void Gamepad_GamepadAdded(object sender, Gamepad e)
+{
+    this._gamePad = e;
+
+    this._run = new Task(this.ReadingGamepad);
+    this._run.Start();
+}
+
+/// <summary>
+/// Liest die den Xbox Controller aus. Für den Vorgang wird eine Schleife verwendet.
+/// </summary>
+private async void ReadingGamepad()
+{
+    while (true)
+    {
+        if(this._gamePad == null) { break; }
+
+        // Aktuelle Eingabe erfassen.
+        GamepadReading gp = this._gamePad.GetCurrentReading();
+                
+        // UI und der Task laufen in verschiedenen Ebenen. 
+        // Daher muss über den Dispatcher die Eingabe Daten 
+        // und die Daten aus dem ViewModel Bindung Async behandelt werden.
+        await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+        {
+            // Linken Stick einlesen
+            var left = gp.LeftThumbstickX;
+            var top = gp.LeftThumbstickY * -1;
+
+            // Abstand zum Rand
+            var distanceToBorder = 30;
+
+            // Eingabe zu einer Position auf der Anwendung umrechnen.
+            var halfLeft = (this.ActualWidth - (distanceToBorder * 2)) / 2d;
+            left = halfLeft + (left * halfLeft);
+            var halfTop = (this.ActualHeight - distanceToBorder * 2) / 2d;
+            top = halfTop + (top * halfTop);
+
+            // verhindern, dass der Punkt den Rand nicht erreicht.
+            if (left < distanceToBorder) { left = distanceToBorder; }
+            if (left > this.ActualWidth) { left = this.ActualWidth; }
+            if (top < distanceToBorder) { top = distanceToBorder; }
+            if (top > this.ActualHeight - distanceToBorder) { top = this.ActualHeight - distanceToBorder; }
+
+            // Position als Text Ausgeben mit bis zu zwei Stellen hinter dem Komma.
+            this._viewModel.TextPosition = $"LEFT: {left:N2}, TOP: {top:N2}";
+
+            // leider kein Binding, daher die simple zuweisung der Ziel Position und Farbe
+            this.MovingPoint.Margin = new Thickness(left, top, 0, 0);
+                    
+            // Farbe wechseln
+            if (gp.Buttons == GamepadButtons.A)
+            {
+                // verkürzte die schreibweise
+                byte f() => (byte)this._random.Next(0, 255);
+                this.MovingPoint.Fill = new SolidColorBrush(Color.FromArgb(255, f(), f(), f()));
+            }
+        });
+          
+        // 2 Millisekunden interval
+        await Task.Delay(2);
+    }
+}
+    }
+}
---
+++ ExampleUwpAndXboxController/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
+
+namespace ExampleUwpAndXboxController
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _textPosition;
+        public string TextPosition {
+            get => this._textPosition;
+            set
+            {
+                this._textPosition = value;
+                this.OnPropertyChanged(nameof(this.TextPosition));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyname) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+    }
+}
